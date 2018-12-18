f63ff5f8196831c8b21eaeb652689c086badd0f6
Alexandre Costa <alex.callum.c@gmail.com>
2018-12-18 23:01:32 +0000

    feat: finish adding validation to user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,29 +1,66 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 const Schema = mongoose.Schema;
+const Joi = require('joi');
+
+// Add enum for user roles
+
+//Creating custom schema for locations for query performance
+const LocationSchema = new Schema ({
+    //No extra validation needed since information comes from locations' original documents
+    name: {
+        type: String,
+        required: true
+    }
+});
 
 const UserSchema = new Schema({
     employeeID: {
-        type: String
+        type: String,
+        minlength: 3,
+        maxlength: 50
     },
     email: {
         type: String,
         unique: true,
-        required: 'Email is required',
-        match: [/.+\@.+\..+/, "Please fill a valid e-mail address"]
+        required: true,
+        minlength: 10,
+        maxlength: 255
     },
-    hashedPassword: String,
-    salt: String,
-    firstName: String,
-    lastName: String,
+    hashedPassword: {
+        type: String,
+        required: true,
+        maxlength: 2048
+    },
+    salt: {
+        type: String,
+        required: true,
+        maxlength: 2048
+    },
+    firstName: {
+        type: String,
+        minlength: 3,
+        maxlength: 255,
+        required: true
+    },
+    lastName: {
+        type: String,
+        minlength: 3,
+        maxlength: 255,
+        required: true
+    },
     company:{
-        type: Schema.Types.ObjectId,
-        ref: 'Company'
+        type: new Schema({
+            //No extra validation needed since info comes from company's document
+            name: {
+                type: String,
+                required: true
+            }
+        })
     },
     locations: {
-        type: [Schema.Types.ObjectId],
-        ref: 'Location',
-        default: undefined
+        type: [LocationSchema],
+        required: true
     }
 });
 
@@ -44,4 +81,28 @@ UserSchema.methods.authenticate = function(password){
     return this.hashedPassword === this.hashPassword(password);
 };
 
-mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
+
+//Creating client input validation method
+const validateUser = (user) => {
+    //creating location item schema
+    const locationItem = {
+        locationId: Joi.objectId().required()
+    };
+
+    const mainInput = {
+        employeeID: Joi.string().min(3).max(50),
+        email: Joi.string().min(10).max(255).required().email(),
+        password: Joi.string().min(8).max(255).required(),
+        firstName: Joi.string().min(3).max(255).required(),
+        lastName: Joi.string().min(3).max(255).required(),
+        companyId: Joi.objectId().required(),
+        locations: Joi.array().items(locationItem).min(1)
+    };
+
+    return Joi.validate(user, mainInput);
+};
+
+//exporting the model and the validation method
+exports.User = User;
+exports.validate = validateUser;

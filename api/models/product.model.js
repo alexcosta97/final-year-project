3e1a898a3e4c83a2c5acf75fa6a823e651458ba9
Alexandre Costa <alex.callum.c@gmail.com>
2018-12-18 17:36:35 +0000

    feat: finish product model and validation

--- api/models/product.model.js
+++ api/models/product.model.js
@@ -1,26 +1,61 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Joi = require('joi')
 
 const ProductSchema = new Schema({
     name:{
         type: String,
-        required: 'Please enter the name of the product'
+        required: true,
+        minlength: 3,
+        maxlength: 255,
+        trim: true
     },
     price:{
         type: Number,
-        required: 'Please enter an item price'
+        required: true,
+        min: 0
     },
     quantity:{
         type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 50,
         default: '1'
     },
     supplierReference:{
-        type: String
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50
     },
+    // Creating a custom schema for supplier for query performance optimization
     supplier:{
-        type: Schema.Types.ObjectId,
-        ref: 'Supplier'
+        type: new Schema({
+            //No extra validation parameters required since the data comes from the original supplier's document
+            name: {
+                type: String,
+                required: true
+            }
+        }),
+        required: true
     }
 });
 
-mongoose.model('Product', ProductSchema);
+const Product = mongoose.model('Product', ProductSchema);
+
+//Creating Joi validation function
+const validateProduct = (product) => {
+    //Creating joi-specific schema for client input validation
+    const schema = {
+        name: Joi.string().min(3).max(255).required(),
+        price: Joi.number().min(0).required(),
+        quantity: Joi.string().min(1).max(50),
+        supplierReference: Joi.string().min(3).max(50).required(),
+        supplierId: Joi.objectId().required()
+    };
+
+    return Joi.validate(product, schema);
+};
+
+exports.Product = Product;
+exports.validate = validateProduct;

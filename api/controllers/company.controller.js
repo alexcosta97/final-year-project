e648fcb48f45f9e026abd985f8e5ef343b5b6a9f
Alexandre Costa <alex.callum.c@gmail.com>
2018-12-31 11:25:55 +0000

    feat: catch castError

--- api/controllers/company.controller.js
+++ api/controllers/company.controller.js
@@ -1,11 +1,20 @@
 const {Company} = require('../models/company.model');
 const Fawn = require('fawn');
 const mongoose = require('mongoose');
+const Joi = require('../config/joi');
 
 Fawn.init(mongoose);
 
+//Create validation methods
+
+
 const get = async (req, res) => {
-    const company = await Company.findOne({_id: req.params.id}).exec();
+    let company;
+    try{
+        company = await Company.findOne({_id: req.params.id}).exec();
+    } catch(err) {
+        return res.status(400).send('Please enter a valid ID');
+    }
 
     if(!company || company === null){
         return res.status(404).send('There was no company with the given ID');

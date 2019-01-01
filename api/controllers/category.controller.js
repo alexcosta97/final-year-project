1614e229312f71a615e2280df9f1ab4d1beb4b65
Alexandre Costa <alex.callum.c@gmail.com>
2019-01-01 04:44:38 +0000

    feat: #25, #26, #27, #28, #32 implement categories controller methods

---
+++ api/controllers/category.controller.js
@@ -0,0 +1,114 @@
+const {Category, validate} = require('../models/category.model');
+const {Company} = require('../models/company.model');
+const mongoose = require('mongoose');
+
+const readAll = async (req, res) => {
+    let categories;
+    try{
+        categories = await Category.find({}).sort('name').exec();
+    }
+    catch(err){
+        return res.status(409).json({message: 'There was an issue processing your request'});
+    }
+
+    res.json(categories);
+};
+
+const read = async (req, res) => {
+    let category;
+    try{
+        category = await Category.findOne({_id: req.params.id}).exec();
+    } catch(err){
+        return res.status(418).json({message: `I'm a teapot. Don't ask me to brew coffee.`});
+    }
+
+    if(!category || category === null){
+        return res.status(404).json({message: 'There was no category with the given ID.'});
+    }
+
+    res.json(category);
+};
+
+const create = async (req, res) => {
+    const {error} = validate(req.body);
+    if(error) return res.status(400).json({message: error.details[0].message});
+    
+
+    let company;
+    try{
+        company = await Company.findById(req.body.companyId);
+    }catch(err){
+        return res.status(400).json({message: 'Invalid Company'});
+    }
+    if(!company) return res.status(400).json({message: 'Invalid Company'});
+
+    let category = new Category({
+        name: req.body.name,
+        company: {
+            _id: company._id,
+            name: company.name
+        }
+    });
+    await category.save();
+    res.json(category);
+};
+
+const update = async (req, res) => {
+    let category;
+    const {error} = validate(req.body);
+    if(error) return res.status(400).json({message: error.details[0].message});
+
+    let company;
+    try{
+        company = await Company.findById(req.body.companyId);
+    }catch(err){
+        return res.status(400).json({message: 'Invalid Company'});
+    }
+
+    try{
+        category = await Category.findOneAndUpdate({_id: req.params.id}, {
+            name: req.body.name,
+            company: {
+                _id: company._id,
+                name: company.name
+            }
+        }, {new: false}).exec();
+    }
+    catch(err){
+        return res.status(418).json({message: `I'm a teapot. Don't ask me to brew coffee.`});
+    }
+
+    if(!category)
+    {
+        return res.status(404).json({message: `There was no category with the given ID.`});
+    }
+
+    res.json({
+        message: 'The operation was successful.'
+    });
+};
+
+const del = async (req, res) => {
+    let category;
+
+    try{
+        category = await Category.findOneAndDelete({_id: req.params.id}).exec();
+    }
+    catch(err){
+        return res.status(418).json({message: `I'm a teapot. Don't ask me to brew coffee.`});
+    }
+
+    if(!category){
+        return res.status(404).json({message: `There was no category with the given ID`});
+    }
+
+    res.json({
+        message: 'The operation was successful.'
+    });
+};
+
+exports.readAll = readAll;
+exports.read = read;
+exports.create = create;
+exports.update = update;
+exports.del = del;
--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,29 +1,10 @@
-//Requiring third party models
 const router = require('express').Router();
+const categories = require('../controllers/category.controller');
 
-//Requiring model and validation methods
-const {Category, validate} = require('../models/category.model');
-
-router.get('/', async (req, res) => {
-    const categories = await Category.find({}).exec();
-    res.send(categories);
-});
-/* router.get('/:id', categories.getOne); */
-router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
-    if(error){
-        return res.status(400).send(error.details[0].message);
-    }
-
-    let category = new Category({
-        name: req.body.name,
-        company: req.body.company
-    });
-    category = await category.save();
-
-    res.send(category);
-});
-/* router.put('/:id', categories.update);
-router.delete('/:id', categories.delete); */
+router.get('/', categories.readAll);
+router.get('/:id', categories.read);
+router.post('/', categories.create);
+router.put('/:id', categories.update);
+router.delete('/:id', categories.del);
 
 module.exports = router;

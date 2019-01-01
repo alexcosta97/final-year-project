acf3584a05320c62b250a844856410bf194f3433
Alexandre Costa <alex.callum.c@gmail.com>
2019-01-01 03:09:59 +0000

    feat: implement update controller method for suppliers

--- api/routes/suppliers.js
+++ api/routes/suppliers.js
@@ -4,7 +4,7 @@ const suppliers = require('../controllers/supplier.controller');
 router.get('/', suppliers.readAll);
 router.get('/:id', suppliers.read);
 router.post('/', suppliers.create);
-// router.put('/:id', suppliers.update);
+router.put('/:id', suppliers.update);
 // router.delete('/:id', suppliers.delete);
 
 module.exports = router;

e671a737cdafc117a3fdd3e517d396b07324efcd
Alexandre Costa <alex.callum.c@gmail.com>
2019-01-01 03:15:58 +0000

    feat: #19 write the delete controller method for suppliers

--- api/routes/suppliers.js
+++ api/routes/suppliers.js
@@ -5,6 +5,6 @@ router.get('/', suppliers.readAll);
 router.get('/:id', suppliers.read);
 router.post('/', suppliers.create);
 router.put('/:id', suppliers.update);
-// router.delete('/:id', suppliers.delete);
+router.delete('/:id', suppliers.del);
 
 module.exports = router;

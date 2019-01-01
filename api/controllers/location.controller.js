b722f2792bb5a9f400c1ce282d64da2594103ba4
Alexandre Costa <alex.callum.c@gmail.com>
2019-01-01 03:44:52 +0000

    feat: #21 implement read location controller method

--- api/controllers/location.controller.js
+++ api/controllers/location.controller.js
@@ -13,4 +13,20 @@ const readAll = async (req, res) => {
     res.json(locations);
 };
 
+const read = async (req, res) => {
+    let location;
+    try{
+        location = await Location.findOne({_id: req.params.id}).exec();
+    } catch(err){
+        return res.status(418).json({message: `I'm a teapot. Don't ask me to brew coffee.`});
+    }
+
+    if(!location || location === null){
+        return res.status(404).json({message: 'There was no location with the given ID.'});
+    }
+
+    res.json(location);
+};
+
 exports.readAll = readAll;
+exports.read = read;

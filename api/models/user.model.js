0739739a6d2fb396a46a1869143fd9d6564903e2
Alexandre Costa <alex.callum.c@gmail.com>
2019-01-05 13:30:22 +0000

    feat: #52 create use model method for hashing

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 const Joi = require('../config/joi');
 
@@ -58,6 +58,41 @@ const UserSchema = new Schema({
         required: true
     }
 });
+
+// Method that automatically hashes passwords before saving
+UserSchema.pre('save', async function(next){
+    if(this.password){
+        console.log(this.password);
+        this.password = await this.hashPassword(this.password);
+    }
+    next();
+});
+
+UserSchema.pre('update', async function(next){
+    if(this.password){
+        this.password = await this.hashPassword(this.password);
+    }
+    next();
+});
+
+UserSchema.pre('findOneAndUpdate', async function(next){
+    if(this.password){
+        this.password = await this.hashPassword(this.password);
+    }
+    next();
+});
+
+UserSchema.pre('updateOne', async function(next){
+    if(this.password){
+        this.password = await this.hashPassword(this.password);
+    }
+    next();
+});
+
+UserSchema.methods.hashPassword = async function(password){
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+};
 
 const User = mongoose.model('User', UserSchema);
 

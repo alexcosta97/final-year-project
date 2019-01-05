777af1b3b0847ae60900b3954c39005645efb017
Alexandre Costa <alex.callum.c@gmail.com>
2019-01-05 15:28:27 +0000

    feat: #53 create token validation middleware

---
+++ api/services/tokenAuth.js
@@ -0,0 +1,24 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+exports.middleware = (req, res, next) => {
+    const token = req.header('x-auth-token');
+    if(!token) return res.status(401).json({message: 'Access denied. No token provided.'});
+
+    const decoded = decoder(token);
+    if(decoded !== Error){
+        req.user = decoded;
+        next()
+    } else return res.status(400).json({message: decoded.message});
+};
+
+const decoder = (token) => {
+    try{
+        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        return decoded;
+    } catch(err){
+        return new Error('Invalid Token');
+    }
+};
+
+exports.decoder = decoder;
